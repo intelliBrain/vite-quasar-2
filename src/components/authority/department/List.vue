60c5c5b496e0b63a553f5ffac19c120a88dd9f93
Young <allen@gmail.com>
2021-07-16 10:34:47 +0800

    解决冲突

--- src/components/authority/department/List.vue
+++ src/components/authority/department/List.vue
@@ -16,16 +16,18 @@
       <div>
         <q-btn class="q-mx-md" @click="editDepartment">设置</q-btn>
         <q-btn @click="createDepartment">添加子部门</q-btn>
+        <UserList />
       </div>
-      <UserList />
     </section>
-    <department-dialog
-      @confirm="onConfirm"
-      @close="onClose"
-      :currentDepartment="currentDepartment"
-      :parentDepartment="parentDepartment"
-      v-if="showDialog"
-    ></department-dialog>
+    <section>
+      <department-dialog
+        @confirm="onConfirm"
+        @close="onClose"
+        :currentDepartment="currentDepartment"
+        :parentDepartment="parentDepartment"
+        v-if="showDialog"
+      ></department-dialog>
+    </section>
   </article>
 </template>
 
@@ -33,16 +35,82 @@
 import { ref, onMounted, computed } from 'vue'
 import { departmentApi } from '@/api/department.js'
 import DepartmentDialog from '@/components/DepartmentDialog.vue'
+import UserList from 'src/components/authority/user/List.vue'
 import utils from '@/util/utils.js'
 import { useQuasar } from 'quasar'
-import UserList from 'src/components/authority/user/List.vue'
 export default {
-  components: {
-    DepartmentDialog,
-    UserList
-  },
+  components: { DepartmentDialog, UserList },
   setup() {
-    return {}
+    const $q = useQuasar()
+    const showDialog = ref(false)
+    const departmentList = ref([])
+    const departmentTree = ref([])
+    const currentDepartment = ref([])
+    const departmentId = ref('')
+
+    const department = computed(
+      () => departmentList.value.filter((dep) => dep.id == departmentId.value)[0]
+    )
+    const parentDepartment = computed(
+      () =>
+        departmentList.value.filter((dep) => dep.id == department.value.parentId)[0] || {
+          name: '公司'
+        }
+    )
+
+    const search = () => {
+      departmentApi.search().then((res) => {
+        departmentList.value = res.data
+        departmentTree.value = utils.buildTree(departmentList.value)
+      })
+    }
+    const createDepartment = () => {
+      if (!departmentId.value) {
+        $q.notify({
+          type: 'warning',
+          message: '请先选择部门'
+        })
+      } else {
+        currentDepartment.value = {
+          name: department.value.name
+        }
+
+        showDialog.value = true
+      }
+    }
+    const editDepartment = () => {
+      if (!departmentId.value) {
+        $q.notify({
+          type: 'warning',
+          message: '请先选择部门'
+        })
+      } else {
+        currentDepartment.value = department.value
+        showDialog.value = true
+      }
+    }
+    const onConfirm = (dep) => {
+      showDialog.value = false
+    }
+    const onClose = () => {
+      showDialog.value = false
+    }
+    onMounted(() => {
+      search()
+    })
+    return {
+      departmentList,
+      departmentTree,
+      editDepartment,
+      createDepartment,
+      onConfirm,
+      onClose,
+      department,
+      parentDepartment,
+      currentDepartment,
+      departmentId,
+      showDialog
+    }
   }
 }
 </script>
