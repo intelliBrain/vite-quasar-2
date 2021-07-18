50363d014f737d03aa8899ccf660424d5443683f
feng <wuyuefeng@shimi-teach.com>
2021-07-18 10:21:02 +0800

    角色

---
+++ src/components/authority/role/Dialog.vue
@@ -0,0 +1,126 @@
+<template>
+  <div>
+    <q-dialog v-model="show" persistent>
+      <q-card style="min-width: 450px">
+        <q-card-section>角色管理 </q-card-section>
+        <q-card-section>
+          <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+            <q-input
+              filled
+              v-model="form.name"
+              dense
+              label="角色名称"
+              hint="角色名称"
+              lazy-rules
+              :rules="[
+                (val) => (val && val.length > 0) || '请填写角色名称',
+                (val) => val.length < 20 || '部门角色长度过长',
+                checkName
+              ]"
+            />
+            <q-field
+              borderless
+              label="权限"
+              :model-value="form.privileges"
+              :rules="[(val) => val.length || '至少选择一个权限']"
+              stack-label
+            >
+              <q-option-group
+                name="accepted_genres"
+                v-model="form.privileges"
+                :options="privileges"
+                type="checkbox"
+                color="primary"
+                inline
+              />
+            </q-field>
+            <div>
+              <q-btn label="确认" type="submit" color="primary" />
+              <q-btn label="删除" @click="deleteRole" class="ml-sm" color="red" v-if="form.id" />
+              <q-btn label="取消" type="reset" color="primary" flat class="ml-sm" />
+            </div>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+import { ref, reactive, toRefs, onMounted } from 'vue'
+import { useQuasar } from 'quasar'
+import { roleApi } from '@/api/user.js'
+export default {
+  props: {
+    role: {
+      type: Object,
+      required: true
+    }
+  },
+  emits: ['confirm', 'close'],
+  setup(props, context) {
+    const $q = useQuasar()
+    const { role } = toRefs(props)
+    const state = reactive({
+      form: {}
+    })
+    const show = ref(true)
+    const onSubmit = () => {
+      state.form.privileges = state.form.privileges.map((item) => ({ code: item }))
+      let promise = null
+      if (state.form.id) {
+        promise = roleApi.update(state.form)
+      } else {
+        promise = roleApi.create(state.form)
+      }
+      promise.then((res) => {
+        $q.notify({
+          type: 'positive',
+          message: state.form.id ? '修改角色成功' : '新建角色成功'
+        })
+        context.emit('confirm', res.data)
+      })
+    }
+    const onReset = () => {
+      context.emit('close')
+    }
+    const deleteRole = () => {
+      console.log('todo delete')
+    }
+    const checkName = (val) => {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          roleApi.findByName(val).then((res) => {
+            let data = res.data
+            if (data && data.id != state.form.id) {
+              resolve('角色名称重复，不可用')
+            } else {
+              resolve(true)
+            }
+          })
+        }, 350)
+      })
+    }
+    const privileges = ref([
+      { value: 'user', label: '用户管理' },
+      { value: 'log', label: '日志管理' },
+      { value: 'authority', label: '权限管理' }
+    ])
+    onMounted(() => {
+      state.form = JSON.parse(JSON.stringify(role.value))
+      state.form.privileges = state.form.privileges.map((item) => item.code)
+    })
+    return {
+      ...toRefs(state),
+      checkName,
+      show,
+      onSubmit,
+      deleteRole,
+      onReset,
+      privileges
+    }
+  }
+}
+</script>
+
+<style></style>
--- src/components/authority/role/List.vue
+++ src/components/authority/role/List.vue
@@ -1,37 +1,36 @@
 <template>
   <article class="page">
-    <section class="aside">
-      <div class="q-pa-md">
-        <q-list bordered separator>
-          <q-item clickable v-ripple>
-            <q-item-section>Single line item</q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple>
-            <q-item-section>
-              <q-item-label>Item with caption</q-item-label>
-              <q-item-label caption>Caption</q-item-label>
-            </q-item-section>
-          </q-item>
-
-          <q-item clickable v-ripple>
-            <q-item-section>
-              <q-item-label overline>OVERLINE</q-item-label>
-              <q-item-label>Item with caption</q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-list>
+    <section class="aside no-padding bg-grey-2">
+      <div class="text-sm text-gray-500">
+        <div
+          class="p-sm cursor-pointer item"
+          :class="{ active: item.id == selectedRole.id }"
+          v-for="(item, index) in roles"
+          :key="index"
+          @click="selectedRole = item"
+        >
+          {{ item.name }}
+        </div>
+        <div class="btn-add" @click="createRole">新增角色</div>
       </div>
     </section>
     <section class="main">
       <div>
-        <span class="text-h6 m-md">超级管理员</span>
+        <span class="text-h6 m-md">{{ selectedRole.name || '' }}</span>
       </div>
       <div class="bg-grey-2 py-sm pl-md m-md">
-        <q-btn color="primary" size="md" @click="create">角色编辑</q-btn>
+        <q-btn color="primary" size="md" @click="editRole">角色编辑</q-btn>
       </div>
       <div class="m-md">
-        <q-table :columns="columns" :rows="rows" row-key="index" class="no-box-shadow">
+        <q-table
+          :columns="columns"
+          :rows="selectedRole.privileges"
+          row-key="index"
+          class="no-box-shadow"
+          no-data-label="该角色暂无权限，请为其添加权限。"
+          hide-pagination
+          :rows-per-page-options="[0]"
+        >
           <template v-slot:body="props">
             <q-tr :props="props">
               <q-td key="index" :props="props">
@@ -48,12 +47,53 @@
         </q-table>
       </div>
     </section>
+    <section>
+      <RoleDialog v-if="showDialog" :role="role" @confirm="onConfirm" @close="onClose" />
+    </section>
   </article>
 </template>
 
 <script>
+import { onMounted, ref } from 'vue'
+import RoleDialog from '@/components/authority/role/Dialog.vue'
+import { roleApi } from '@/api/user.js'
 export default {
+  components: { RoleDialog },
   setup() {
+    const roles = ref([])
+    const listRoles = () => {
+      roleApi.list().then((res) => {
+        roles.value = res.data
+        selectedRole.value = roles.value[0]
+      })
+    }
+    const role = ref({})
+    const selectedRole = ref({})
+    const showDialog = ref(false)
+    const createRole = () => {
+      role.value = {
+        name: '',
+        privileges: []
+      }
+      showDialog.value = true
+    }
+    const editRole = () => {
+      role.value = selectedRole.value
+      showDialog.value = true
+    }
+    const onConfirm = (data) => {
+      if (!role.value.id) {
+        roles.value.push(data)
+      } else {
+        let index = roles.value.findIndex((item) => item.id == data.id)
+        roles.value.splice(index, 1, data)
+      }
+      selectedRole.value = data
+      showDialog.value = false
+    }
+    const onClose = () => {
+      showDialog.value = false
+    }
     const columns = [
       {
         name: 'index',
@@ -64,63 +104,45 @@ export default {
       { name: 'name', label: '权限名', field: 'name', align: 'left' },
       { name: 'description', label: '权限描述', field: 'description' }
     ]
-
-    const rows = [
-      {
-        name: '写文章',
-        description: '写文章写文章写文章写文章写文章写文章写文章写文章写文章写文章写文章'
-      },
-      {
-        name: '文章审核',
-        description: '文章审核文章审核文章审核文章审核文章审核文章审核文章审核'
-      },
-      {
-        name: 'Eclair',
-        description: 262
-      },
-      {
-        name: 'Cupcake',
-        description: 305
-      },
-      {
-        name: 'Gingerbread',
-        description: 356
-      },
-      {
-        name: 'Jelly bean',
-        description: 375
-      },
-      {
-        name: 'Lollipop',
-        description: 392
-      },
-      {
-        name: 'Honeycomb',
-        description: 408
-      },
-      {
-        name: 'Donut',
-        description: 452
-      },
-      {
-        name: 'KitKat',
-        description: 518
-      },
-      {
-        name: '写文章',
-        description: '写文章写文章写文章写文章写文章写文章写文章写文章写文章写文章写文章'
-      },
-      {
-        name: '文章审核',
-        description: '文章审核文章审核文章审核文章审核文章审核文章审核文章审核'
-      }
-    ]
-    const create = () => {
-      console.log('create')
+    onMounted(() => {
+      listRoles()
+    })
+    return {
+      listRoles,
+      roles,
+      role,
+      selectedRole,
+      columns,
+      createRole,
+      editRole,
+      onConfirm,
+      onClose,
+      showDialog
     }
-    return { columns, rows, create }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.item {
+  border-bottom: 1px solid #e7e7e7;
+  &.active {
+    background: #027be3;
+    color: #fff;
+  }
+}
+
+.btn-add {
+  position: absolute;
+  bottom: 0;
+  height: 44px;
+  left: 0;
+  line-height: 44px;
+  background: #ebebeb;
+  text-align: center;
+  width: 300px;
+  font-size: 14px;
+  font-weight: 500;
+  cursor: pointer;
+}
+</style>
