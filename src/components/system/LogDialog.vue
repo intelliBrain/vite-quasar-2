9ee619396ab23aede78c96995eade20ecf261df4
maojx <maojunxiang@shimi-tech.com>
2021-07-19 20:02:35 +0800

    日志模块优化

---
+++ src/components/system/LogDialog.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-dialog v-model="fixed">
+    <q-card>
+      <q-card-section>
+        <div class="text-h6">查看</div>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-section style="max-height: 50vh" class="scroll">
+        <vue-json-pretty :data="jsonData"> </vue-json-pretty>
+      </q-card-section>
+
+      <q-separator />
+
+      <q-card-actions align="right">
+        <q-btn flat label="关闭" color="primary" @click="close" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+import { ref } from 'vue'
+import VueJsonPretty from 'vue-json-pretty'
+import 'vue-json-pretty/lib/styles.css'
+
+export default {
+  components: {
+    VueJsonPretty
+  },
+  setup(props, context) {
+    const fixed = ref(true)
+    const jsonData = ref(null)
+    jsonData.value = JSON.parse(
+      '{"id":1,"createdTime":"2021-06-09 16:49:59","updatedTime":"2021-06-09 16:49:59","createdBy":{"id":1,"createdTime":null,"updatedTime":null,"createdBy":null,"updatedBy":null,"uid":"L7ceIEE0zVof","nickname":"superadmin","username":"superadmin","rawPassword":null,"smsCode":null,"phone":null,"email":null,"gender":null,"avatar":"http://static.tymetro.ltd/tymetro/Ty7y763h5x671fVijNccNMMuOEkM3o.png","newUsername":null,"enabled":true,"locked":false,"lastLoginIp":null,"roles":[],"captcha":null,"lastLoginTime":null,"passwordExpired":true,"department":null,"userBinds":[],"superAdmin":true,"admin":true},"updatedBy":null,"name":"a1","count":2,"discoveryTime":"2021-06-09 16:30:00","telephone":"18605711001","place":"h","status":"DRAFT","retrieve":false,"publishedTime":null,"revokedTime":null}'
+    )
+
+    const close = () => {
+      context.emit('close')
+    }
+    return {
+      fixed,
+      jsonData,
+      close
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/system/log.vue
+++ src/pages/system/log.vue
@@ -1,53 +1,101 @@
 <template>
   <q-page>
-    <div class="main">
-      <div class="main-header">
-        <q-form @submit="onSubmit" @reset="onReset">
-          <q-input filled v-model="date1" mask="date" :rules="['date']" label="操作时间（始）">
-            <template v-slot:prepend>
-              <q-icon name="event" class="cursor-pointer"> </q-icon>
-              <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
-                <q-date v-model="date1">
+    <q-form @submit="onSubmit" @reset="onReset">
+      <div class="row">
+        <q-input outlined v-model="date1" label="操作时间（始）" class="p-sm flex-1" dense>
+          <template v-slot:prepend>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy transition-show="scale" transition-hide="scale">
+                <q-date v-model="date1" mask="YYYY-MM-DD">
                   <div class="row items-center justify-end">
                     <q-btn v-close-popup label="Close" color="primary" flat />
                   </div>
                 </q-date>
               </q-popup-proxy>
-            </template>
-          </q-input>
-          <q-input filled v-model="date2" mask="date" :rules="['date']" label="操作时间（终）">
-            <template v-slot:prepend>
-              <q-icon name="event" class="cursor-pointer"> </q-icon>
-              <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
-                <q-date v-model="date2">
+            </q-icon>
+          </template>
+        </q-input>
+        <q-input outlined v-model="date1" label="操作时间（终）" class="p-sm flex-1" dense>
+          <template v-slot:prepend>
+            <q-icon name="event" class="cursor-pointer">
+              <q-popup-proxy transition-show="scale" transition-hide="scale">
+                <q-date v-model="date1" mask="YYYY-MM-DD">
                   <div class="row items-center justify-end">
                     <q-btn v-close-popup label="Close" color="primary" flat />
                   </div>
                 </q-date>
               </q-popup-proxy>
-            </template>
-          </q-input>
-
-          <q-select outlined v-model="model1" :options="md" label="模块" />
-
-          <q-input outlined bottom-slots v-model="text" label="消息">
-            <template v-slot:prepend>
-              <q-icon name="search" />
-            </template>
-          </q-input>
-          <q-input outlined bottom-slots v-model="text1" label="姓名或者账号">
-            <template v-slot:prepend>
-              <q-icon name="search" />
-            </template>
-          </q-input>
-        </q-form>
+            </q-icon>
+          </template>
+        </q-input>
+        <q-select outlined v-model="model" :options="md" label="模块" class="p-sm flex-1" dense />
+        <q-input outlined bottom-slots v-model="text" label="消息" class="p-sm flex-1" dense>
+          <template v-slot:prepend>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+        <q-input
+          outlined
+          bottom-slots
+          v-model="text1"
+          label="姓名或者账号"
+          class="p-sm flex-1"
+          dense
+        >
+          <template v-slot:prepend>
+            <q-icon name="search" />
+          </template>
+        </q-input>
       </div>
-      <div class="main-content">
-        <div class="q-pa-md">
-          <q-table :rows="rows" :columns="columns" row-key="name" />
-        </div>
-      </div>
-      <div class="main-footer"></div>
+    </q-form>
+
+    <div class="q-pa-md">
+      <q-table :columns="columns" :rows="rows" row-key="name" class="no-box-shadow">
+        <template v-slot:body="props">
+          <q-tr :props="props">
+            <q-td key="name" :props="props">
+              {{ props.row.name }}
+            </q-td>
+            <q-td key="calories" :props="props">
+              {{ props.row.calories }}
+            </q-td>
+            <q-td key="fat" :props="props">
+              {{ props.row.fat }}
+            </q-td>
+            <q-td key="carbs" :props="props">
+              {{ props.row.carbs }}
+            </q-td>
+            <q-td key="carbs" :props="props">
+              {{ props.row.protein }}
+            </q-td>
+            <q-td key="optType" :props="props">
+              <q-btn flat color="primary" label="查看详情" @click="viewDetail()" />
+            </q-td>
+          </q-tr>
+        </template>
+        <template v-slot:bottom> </template>
+        <template v-slot:pagination>
+          <span></span>
+        </template>
+      </q-table>
+    </div>
+
+    <div class="q-pa-sm flex justify-end">
+      <q-pagination
+        v-model="current"
+        :max="5"
+        direction-links
+        boundary-links
+        icon-first="skip_previous"
+        icon-last="skip_next"
+        icon-prev="fast_rewind"
+        icon-next="fast_forward"
+        dense
+      />
+    </div>
+
+    <div>
+      <log-dialog v-if="dialogVisible" @close="onClose" />
     </div>
   </q-page>
 </template>
@@ -55,75 +103,101 @@
 <script>
 import { useQuasar } from 'quasar'
 import { ref } from 'vue'
-
-const columns = [
-  {
-    label: '用户',
-    align: 'center',
-    field: (row) => row.name
-  },
-  {
-    label: '模块',
-    align: 'center',
-    field: (row) => row.name
-  },
-  {
-    label: '操作',
-    align: 'center',
-    field: (row) => row.name
-  },
-  {
-    label: '消息',
-    align: 'center',
-    field: (row) => row.name
-  },
-  {
-    label: '操作时间',
-    align: 'center',
-    field: (row) => row.name
-  },
-  {
-    label: '操作',
-    align: 'center',
-    field: (row) => row.name
-  }
-]
-
-const rows = [
-  {
-    name: 'Frozen Yogurt',
-    calories: 159,
-    fat: 6.0,
-    carbs: 24,
-    protein: 4.0,
-    sodium: 87
-  },
-  {
-    name: 'Ice cream sandwich',
-    calories: 237,
-    fat: 9.0,
-    carbs: 37,
-    protein: 4.3,
-    sodium: 129
-  }
-]
-
+import LogDialog from '@/components/system/LogDialog.vue'
 export default {
+  components: {
+    LogDialog
+  },
   setup() {
     const $q = useQuasar()
     const name = ref(null)
     const date1 = ref(null)
     const date2 = ref(null)
+    const dialogVisible = ref(false)
+    const current = ref(3)
+    const columns = [
+      {
+        name: 'name',
+        label: '用户',
+        align: 'center',
+        field: (row) => row.name
+      },
+      {
+        name: 'calories',
+        label: '模块',
+        align: 'center',
+        field: (row) => row.name
+      },
+      {
+        name: 'fat',
+        label: '操作',
+        align: 'center',
+        field: (row) => row.name
+      },
+      {
+        name: 'carbs',
+        label: '消息',
+        align: 'center',
+        field: (row) => row.name
+      },
+      {
+        name: 'protein',
+        label: '操作时间',
+        align: 'center',
+        field: (row) => row.name
+      },
+      {
+        name: 'optType',
+        label: '操作',
+        align: 'center',
+        field: 'optType'
+      }
+    ]
+
+    const rows = [
+      {
+        name: 'Frozen Yogurt',
+        calories: 159,
+        fat: 6.0,
+        carbs: 24,
+        protein: 4.0,
+        optType: 87
+      },
+      {
+        name: 'Ice cream sandwich',
+        calories: 237,
+        fat: 9.0,
+        carbs: 37,
+        protein: 4.3,
+        optType: 129
+      }
+    ]
+
+    const viewDetail = () => {
+      dialogVisible.value = true
+    }
+    const onClose = () => {
+      dialogVisible.value = false
+    }
     return {
       name,
       date1,
       date2,
       md: ['Google', 'Facebook', 'Twitter', 'Apple', 'Oracle'],
       columns,
-      rows
+      rows,
+      logData: {},
+      dialogVisible,
+      viewDetail,
+      onClose,
+      current
     }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.qtb {
+  min-height: 1px;
+}
+</style>
