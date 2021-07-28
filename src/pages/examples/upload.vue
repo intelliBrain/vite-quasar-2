c017dc5cc01301558a3cf013a917845994d3e3bd
Young <allen@gmail.com>
2021-07-28 23:42:04 +0800

    update deps

---
+++ src/pages/examples/upload.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="p-md">
+    <q-uploader
+      url="http://localhost:4444/upload"
+      style="max-width: 300px"
+      hide-upload-btn
+      color="white"
+      flat
+      label="uploader"
+    >
+      <template v-slot:header></template>
+      <template v-slot:list="scope">{{ scope }},list </template>
+    </q-uploader>
+  </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped></style>
