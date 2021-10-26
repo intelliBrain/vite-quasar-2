e9c64925a820ce789c70c5cd3ab6012d03f6fbc1
LiuYang <fyeeme@gmail.com>
2021-10-26 14:27:50 +0800

    feat:  use quasar base on @quasar/vite-plugin

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { Quasar } from 'quasar'
+import QuasarConfig from 'src/plugins/quasar'
+createApp(App).use(Quasar, QuasarConfig).mount('#app')
---
+++ src/plugins/quasar.js
@@ -0,0 +1,21 @@
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
+
+export default {
+  plugins: {}, // import Quasar plugins and add here
+  /*
+  config: {
+    brand: {
+      // primary: '#e46262',
+      // ... or all other brand colors
+    },
+    notify: {...}, // default set of options for Notify Quasar plugin
+    loading: {...}, // default set of options for Loading Quasar plugin
+    loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+    // ..and many more (check Installation card on each Quasar component/directive/plugin)
+  }
+  */
+}
